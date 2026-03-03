656133a65e3fb8ec42942e5766331d98be9dd66d
Ken <kencaspe77@gmail.com>
2026-03-03 09:15:54 +0800

    Clean up JavaScript imports in app.js
    Removed unused JavaScript imports from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,5 @@ import "./bootstrap";
 
 //Javascript
 import "../src/javascript/file-upload";
-import "../src/javascript/loader";
-import "../src/javascript/print";
 import "../src/javascript/authentication";
-import "../src/javascript/motto";
-import "../src/javascript/chart";
 import "../src/javascript/admin-modal";
-
-import "../src/javascript/employer/job";
